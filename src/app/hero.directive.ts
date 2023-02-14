90df4ff45d08c8edde5df3a05e7d2252211ffdca
tauras <taurastaurenas@gmail.com>
2023-02-14 15:16:54 +0200

    add on mouse enter and on mouse leave events that handles background color changes using hostlistener, as well as add default background color using hostbinding

--- src/app/hero.directive.ts
+++ src/app/hero.directive.ts
@@ -1,9 +1,33 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  HostListener,
+  ElementRef,
+  Renderer2,
+  HostBinding,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHero]',
 })
 export class HeroDirective {
-  // Your code here, please
-}
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
+  @HostBinding('style.backgroundColor') background: string = '#c4c3c3';
+
+  @HostListener('mouseenter') onmouseover() {
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'background-color',
+      '#2b76f6'
+    );
+    this.renderer.setStyle(this.element.nativeElement, 'transition', '0.5s');
+  }
+
+  @HostListener('mouseleave') onmouseout() {
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'background-color',
+      '#c4c3c3'
+    );
+  }
+}
